b6ee5e9ad0264270623fa01865b1ef1da5fc19ca
Eli Anderson <dispentia@gmail.com>
2020-08-11 15:30:25 -0500

    join_datatables.py: Formatting

--- join_datatables.py
+++ join_datatables.py
@@ -1,6 +1,7 @@
 from datetime import datetime as dt
 import pandas as pd
 
+
 def join_datatables():
     tp_csv = pd.read_csv("C:/Users/mingus/Documents/treatment_due_dates.csv")
     pw_csv = pd.read_csv("C:/Users/mingus/Documents/primary_workers.csv")
@@ -16,11 +17,13 @@ def join_datatables():
                  'worker_assignment_id', 'worker_start', 'worker_end', 'staff_id',
                  'supervisor_id', 'supervisor_name_y', 'is_primary_worker', 'managing_office_id',
                  'managing_office', 'worker_number', 'unit_number', 'worker_unit', 'prg_days',
-                 'last_date_serv', 'total_dist', 'grand_total_dist', 'worker_role', 'program_name'], axis=1, inplace=True)
-    merged = merged.rename(columns = {'worker_name':'primary_worker'})
+                 'last_date_serv', 'total_dist', 'grand_total_dist', 'worker_role', 'program_name'], axis=1,
+                inplace=True)
+    merged = merged.rename(columns={'worker_name': 'primary_worker'})
     merged['expiration_date'] = pd.to_datetime(merged.expiration_date)
     merged.sort_values(by=['primary_worker', 'expiration_date'], inplace=True, ascending=[True, True])
-    merged.to_csv("C:/Users/mingus/Documents/" + str(dt.now().month+1) + "-" + str(dt.now().year) +
+    merged.to_csv("C:/Users/mingus/Documents/" + str(dt.now().month + 1) + "-" + str(dt.now().year) +
                   "_treatment_plan_due_dates.csv", index=False)
+
 
 join_datatables()
