2503359afc152d54c0b74095719d6c09ae458232
Eli Anderson <dispentia@gmail.com>
2020-08-11 15:04:43 -0500

    claim_details.py: Calculate subtotals for each staff member in each payor group

--- claim_details.py
+++ claim_details.py
@@ -27,21 +27,35 @@ def claim_details():
     csv['total_amount_paid'] = csv['total_amount_paid'].replace('[\$]', '', regex=True).astype(float)
 
     totals = csv.sum(axis=0, skipna=True)
-    total_row = {'invoice_number_x': None, 'payor_name': None, 'actual_date': 'Totals:',
-           'amount_expected': totals['amount_expected'], 'total_amount_paid': totals['total_amount_paid'],
-           'procedure_name': None, 'procedure_code': None, 'full_name': None, 'staff_name': None}
+    total_row = {'invoice_number_x': None, 'payor_name': None, 'actual_date': 'Grand Totals:',
+                 'amount_expected': totals['amount_expected'], 'total_amount_paid': totals['total_amount_paid'],
+                 'procedure_name': None, 'procedure_code': None, 'full_name': None, 'staff_name': None}
+    empty_row = {'invoice_number_x': None, 'payor_name': None, 'actual_date': None,
+                 'amount_expected': None, 'total_amount_paid': None,
+                 'procedure_name': None, 'procedure_code': None, 'full_name': None, 'staff_name': None}
     
-    sums = csv.groupby('payor_name')['amount_expected','total_amount_paid'].sum()
-    for idx, sum in sums.iterrows():
-        row = {'invoice_number_x': None, 'payor_name': None, 'actual_date': idx + ' Subtotal:',
-               'amount_expected': sums.loc[idx]['amount_expected'], 'total_amount_paid': sums.loc[idx]['total_amount_paid'],
-               'procedure_name': None, 'procedure_code': None, 'full_name': None, 'staff_name': None}
-        csv = csv.append(row, ignore_index=True)
-        
-    csv = csv.append(total_row, ignore_index=True)
+    sums = csv.groupby(['payor_name', 'staff_name'])['amount_expected','total_amount_paid'].sum()
+    payors = csv.payor_name.unique()
+    
+    for payor in payors:
+        irow = {'invoice_number_x': None, 'payor_name': payor, 'actual_date': 'Totals:',
+           'amount_expected': sums.loc[payor]['amount_expected'].sum(), 'total_amount_paid': sums.loc[payor]['total_amount_paid'].sum(),
+           'procedure_name': None, 'procedure_code': None, 'full_name': None, 'staff_name': None}
+        csv = csv.append(irow, ignore_index=True)
+        for jdx, jsum in sums.loc[payor].iterrows():
+            jrow = {'invoice_number_x': None, 'payor_name': None, 'actual_date': jdx,
+                   'amount_expected': jsum['amount_expected'], 'total_amount_paid': jsum['total_amount_paid'],
+                   'procedure_name': None, 'procedure_code': None, 'full_name': None, 'staff_name': None}
+            csv = csv.append(jrow, ignore_index=True)
     
     csv['amount_expected'] = csv['amount_expected'].apply(lambda val: '${:,.2f}'.format(val))
     csv['total_amount_paid'] = csv['total_amount_paid'].apply(lambda val: '${:,.2f}'.format(val))
+    
+    csv = csv.append(pd.Series(), ignore_index=True)    
+    csv = csv.append(total_row, ignore_index=True)
+    
+    csv.iloc[len(csv)-1, 3] = '${:,.2f}'.format(csv.iloc[len(csv)-1, 3])
+    csv.iloc[len(csv)-1, 4] = '${:,.2f}'.format(csv.iloc[len(csv)-1, 4])
 
     csv.to_csv(filename, index=False)
     
