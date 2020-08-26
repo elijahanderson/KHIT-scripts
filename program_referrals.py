4365c1d2907664c03ae715357f3452a168a0a44b
Eli Anderson <dispentia@gmail.com>
2020-08-26 12:39:44 -0400

    program_referrals.py: Finalize script for production

--- program_referrals.py
+++ program_referrals.py
@@ -12,20 +12,22 @@ def program_referrals():
     df.to_excel(xl_writer, sheet_name='Program Referrals', index=False)
     worksheet = xl_writer.sheets['Program Referrals']
 
-    plt.rc('font', size=8)
-    figsize = 10.0
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(figsize, figsize))
-
-    df.referral_from.value_counts().plot(kind='pie', label='', ax=ax1)
-    df.referral_reason.value_counts().plot(kind='pie', label='', ax=ax2)
-    ax1.set_title('Referral From')
-    ax2.set_title('Referral Reason')
-    fig.tight_layout()
-    # fig.subplots_adjust(left=1/figsize, right=1-.5/figsize, bottom=1/figsize, top=1-.5/figsize, wspace=0.7)
-    fig.savefig('program_referrals.png')
-    worksheet.insert_image('A20', 'program_referrals.png')
+    # df.referral_from.value_counts().plot(kind='pie', label='', ax=ax1)
+    # df.referral_reason.value_counts().plot(kind='pie', label='', ax=ax2)
+    # ax1.set_title('Referral From')
+    # ax2.set_title('Referral Reason')
+    # fig.tight_layout()
+    # fig.savefig('program_referrals.png')
+    # worksheet.insert_image('A20', 'program_referrals.png')
 
     xl_writer.save()
+
+
+def test():
+    filename = "C:/Users/mingus/Documents/" + str(dt.now().month) + "-" + str(dt.now().year) + "_program_referrals.xlsx"
+
+    df = pd.read_csv('C:/Users/mingus/Documents/program_referrals.csv')
+    df.to_csv(filename)
 
 
 program_referrals()
