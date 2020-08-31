d8a82b9eacc09bade73874a328aaf1e79540c2f5
Eli Anderson <dispentia@gmail.com>
2020-08-31 16:29:37 -0400

    Create crisis_src directory and r30-48.py

---
+++ crisis_src/r30-48.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+
+def r30_48():
+    filename = 'C:/Users/mingus/Documents/r30-48n.csv'
+
+    df = pd.read_csv('C:/Users/mingus/Documents/r30-48.csv')
+    df.sort_values(by=['full_name', 'actual_date'], inplace=True)
+    df = df[df.duplicated(subset=['full_name'], keep=False)]
+    df = df.reset_index()
+
+    by_client = df.groupby('full_name')
+    for client, frame in by_client:
+        if 'Crisis Screening Services' not in frame['program_name'].values:
+            print(frame['program_name'])
+            df = df.drop(frame['program_name'].index)
+
+    df.to_csv(filename, index=False)
+
+    return True
+
+
+r30_48()
