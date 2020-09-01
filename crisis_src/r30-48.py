54c914ee29196a8343308d1787ebf0480d9ce194
Eli Anderson <dispentia@gmail.com>
2020-09-01 16:26:29 -0400

    r30-48.py: Prelim algorithm for converting data from r30-48.csv to crisis_src.xlsx

--- crisis_src/r30-48.py
+++ crisis_src/r30-48.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import time
 
 
 def r30_48():
@@ -12,10 +13,28 @@ def r30_48():
     by_client = df.groupby('full_name')
     for client, frame in by_client:
         if 'Crisis Screening Services' not in frame['program_name'].values:
-            print(frame['program_name'])
             df = df.drop(frame['program_name'].index)
 
-    df.to_csv(filename, index=False)
+    crisis_src = pd.read_excel('C:/Users/mingus/Documents/crisis_src.xlsx')
+    crisis_src = crisis_src.rename(columns={
+        'Name of Designated Screening Center: Oaks Integrated Care       \n\nCounty: Camden': 'desc'})
+    # TODO -- insert current month, not aug_2020
+    crisis_src.insert(loc=crisis_src.columns.get_loc('jun_2020') + 1, column='aug_2020', value=0)
+    crisis_src.loc[44, 'desc'] = 'Involuntary Outpatient Commitment'
+
+    for program in df['program_name']:
+        if program != 'Crisis Screening Services':
+            # TODO -- Go through program list on myEvolv to match the rest up
+            if 'ICMS' in program:
+                crisis_src.loc[34, 'aug_2020'] = crisis_src.loc[34, 'aug_2020'] + 1
+            elif 'Involuntary Outpatient Commitment' in program:
+                crisis_src.loc[44, 'aug_2020'] = crisis_src.loc[44, 'aug_2020'] + 1
+            else:
+                crisis_src.loc[45, 'aug_2020'] = crisis_src.loc[45, 'aug_2020'] + 1
+
+    xl_writer = pd.ExcelWriter('C:/Users/mingus/Documents/crisis_src_2.xlsx', engine='xlsxwriter')
+    crisis_src.to_excel(xl_writer, sheet_name='crisis_src', index=False)
+    xl_writer.save()
 
 
 r30_48()
