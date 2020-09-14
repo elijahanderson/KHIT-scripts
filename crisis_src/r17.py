68a6a7cb4bf83f044862f633e166addc77788310
Eli Anderson <dispentia@gmail.com>
2020-09-14 16:28:31 -0400

    r17.py: Script marked as completed

--- crisis_src/r17.py
+++ crisis_src/r17.py
@@ -3,18 +3,29 @@ from datetime import date, datetime
 
 
 def r17():
+    """
+        -- r17 --
+        description:    Adults discharged to the community after Crisis enrollment for greater than 4 and less than 24
+                        hours.
+        author notes:   May need to filter report to show only discharges to community in the future, but I think that
+                        all discharges are to the community.
+    """
     df = pd.read_csv('C:/Users/mingus/Documents/r17.csv')
+    # sorting required for working algorithm
     df.sort_values(by=['full_name', 'actual_date'], inplace=True)
     df = df.reset_index(drop=True)
 
+    # convert to datetime for date operations
     df["actual_date"] = pd.to_datetime(df.actual_date)
     df["end_date"] = pd.to_datetime(df.end_date)
+    # rename for legibility
     df = df.rename(columns={'actual_date.1': 'start_time', 'end_date.1':'end_time'})
 
     curr_date = date.today().strftime('%b_%Y').lower()
     crisis_src = pd.read_excel('crisis_sfy_2021.xlsx')
     time_format = '%Y-%m-%d %I:%M %p'
 
+    # if the time between actual_date and end_date is >= 4 or <= 24 hours, increment row 17 of the megareport
     for idx, row in df.iterrows():
         start_date = str(row['actual_date'])[0:10] + ' ' + str(row['start_time'])
         end_date = str(row['end_date'])[0:10] + ' ' + str(row['end_time'])
