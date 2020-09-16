a8cc83d5cf6109f2fc68da6e08b82425913b93da
Eli Anderson <dispentia@gmail.com>
2020-09-16 16:25:56 -0400

    r28-29.py: Script marked as complete

--- crisis_src/r28-29.py
+++ crisis_src/r28-29.py
@@ -3,11 +3,18 @@ from datetime import date, datetime
 
 
 def r28_29():
+    """
+            -- r18-20 --
+            description:    Adults with a Crisis Triage event and Outreach event within 1-2 or > 2 hours of each other
+            author notes:   Not a single client that meets specified criteria. Should probably ask Oaks about it.
+    """
     df = pd.read_csv('C:/Users/mingus/Documents/r28-29.csv')
     df.sort_values(by=['full_name', 'actual_date'], inplace=True)
     df = df[df.duplicated(subset=['full_name'], keep=False)]
     df = df.reset_index(drop=True)
+    # convert to pd datetime for date operations
     df["actual_date"] = pd.to_datetime(df.actual_date)
+    # rename for legibility
     df = df.rename(columns={'actual_date': 'date', 'actual_date.1': 'time'})
 
     by_client = df.groupby('full_name')
@@ -15,11 +22,15 @@ def r28_29():
     crisis_src = pd.read_excel('crisis_sfy_2021.xlsx')
     time_format = '%I:%M %p'
 
+    # for each client
     for client, frame in by_client:
         frame = frame.reset_index(drop=True)
-        if 'Outreach' in frame.event_name.unique():
+        # if they have both events
+        if 'Outreach' in frame.event_name.unique() and 'Crisis Triage' in frame.event_name.unique():
             for idx, row in frame.iterrows():
                 if (idx+1) < len(frame):
+                    # look at two rows at a time, and if their date is the same but event is different, then increment
+                    # the appropriate row according to the time difference (1-2 or >=2 hours)
                     if row['date'] == frame.loc[idx+1, 'date'] and row['event_name'] != frame.loc[idx+1, 'event_name']:
                         tdelta = datetime.strptime(row['time'], time_format) - \
                                  datetime.strptime(frame.loc[idx+1, 'time'], time_format)
