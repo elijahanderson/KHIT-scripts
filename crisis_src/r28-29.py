c5ecfba5ee106115b407adccaa7d5d47ed603401
Eli Anderson <dispentia@gmail.com>
2020-09-21 16:45:46 -0400

    r28-29.py: Fix algorithm

--- crisis_src/r28-29.py
+++ crisis_src/r28-29.py
@@ -7,11 +7,9 @@ def r28_29():
     """
         -- r18-20 --
         description:    Adults with a Crisis Triage event and Outreach event within 1-2 or > 2 hours of each other
-        author notes:   Not a single client that meets specified criteria. Should probably ask Oaks about it.
+        author notes:
     """
     df = pd.read_csv('C:/Users/mingus/Documents/r28-29.csv')
-    # drop clients w/ only one event during time period
-    df = df[df.duplicated(subset=['full_name'], keep=False)]
     # drop all clients less than 18 y/o
     today = date.today().strftime('%Y-%m-%d')
     df["dob"] = pd.to_datetime(df.dob)
@@ -24,6 +22,16 @@ def r28_29():
     # rename for legibility
     df = df.rename(columns={'actual_date': 'date', 'actual_date.1': 'time'})
 
+    outreaches = pd.read_csv('C:/Users/mingus/Documents/r6-14.csv')
+    outreaches['actual_date'] = pd.to_datetime(outreaches.actual_date)
+    outreaches = outreaches.rename(columns={'actual_date': 'outreach_date'})
+    outreaches['full_name'] = outreaches['last_name'] + ', ' + outreaches['first_name']
+    outreaches = outreaches[['full_name', 'outreach_date']]
+
+    df = df.merge(outreaches, on='full_name')
+    df.to_csv('C:/Users/mingus/Documents/merged.csv')
+    print(df)
+
     by_client = df.groupby('full_name')
     curr_date = date.today().strftime('%b_%Y').lower()
     crisis_src = pd.read_excel('D:/KHIT docs/KHIT-scripts/crisis_src/crisis_sfy_2021.xlsx')
@@ -32,22 +40,18 @@ def r28_29():
     # for each client
     for client, frame in by_client:
         frame = frame.reset_index(drop=True)
-        # if they have both events
-        if 'Outreach' in frame.event_name.unique() and 'Crisis Triage' in frame.event_name.unique():
-            for idx, row in frame.iterrows():
-                if (idx+1) < len(frame):
-                    # look at two rows at a time, and if their date is the same but event is different, then increment
-                    # the appropriate row according to the time difference (1-2 or >=2 hours)
-                    if row['date'] == frame.loc[idx+1, 'date'] and row['event_name'] != frame.loc[idx+1, 'event_name']:
-                        tdelta = datetime.strptime(row['time'], time_format) - \
-                                 datetime.strptime(frame.loc[idx+1, 'time'], time_format)
-                        hr_diff = tdelta.seconds / 3600
-                        # between 1 & 2 hours
-                        if hr_diff >= 1.0 or hr_diff < 2.0:
-                            crisis_src.loc[26, curr_date] = crisis_src.loc[26, curr_date] + 1
-                        # over 2 hours
-                        elif hr_diff >= 2.0:
-                            crisis_src.loc[27, curr_date] = crisis_src.loc[27, curr_date] + 1
+        for idx, row in frame.iterrows():
+            # increment the appropriate row according to the time difference (1-2 or >=2 hours)
+            if str(row['date'])[0:10] == str(frame.loc[idx, 'outreach_date'])[0:10]:
+                tdelta = datetime.strptime(str(frame.loc[idx, 'outreach_date'])[11:], '%H:%M:%S') - \
+                         datetime.strptime(row['time'], time_format)
+                hr_diff = tdelta.seconds / 3600
+                # between 1 & 2 hours
+                if 1.0 <= hr_diff < 2.0:
+                    crisis_src.loc[26, curr_date] = crisis_src.loc[26, curr_date] + 1
+                # over 2 hours
+                elif hr_diff >= 2.0:
+                    crisis_src.loc[27, curr_date] = crisis_src.loc[27, curr_date] + 1
 
     # sum each program
     for idx, row in crisis_src.loc[26:27, :].iterrows():
