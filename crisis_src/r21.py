757c4c8b598dc19ad1fb197ad423e3de771b0cb3
Eli Anderson <dispentia@gmail.com>
2020-11-04 20:24:42 -0500

    r21: Remove personal algorithm and redo script to use Oaks SQL-based report

--- crisis_src/r21.py
+++ crisis_src/r21.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from datetime import date, datetime
+from datetime import date, timedelta
 
 
 def r21():
@@ -11,43 +11,13 @@ def r21():
                         requirements.
     """
     df = pd.read_csv('C:/Users/mingus/Documents/r21.csv')
-    df = df.dropna(subset=['end_date'], axis=0)
-    df["actual_date"] = pd.to_datetime(df.actual_date)
-    df["end_date"] = pd.to_datetime(df.end_date)
-    # drop all clients less than 18 y/o
-    today = date.today().strftime('%Y-%m-%d')
-    df["dob"] = pd.to_datetime(df.dob)
-    df['dob'] = df['dob'].apply(lambda dob: (pd.to_datetime(today) - dob) / np.timedelta64(1, 'Y'))
-    df = df[df['dob'] > 18]
-    df.sort_values(by=['full_name', 'actual_date'], inplace=True)
     # Drop unique rows -- target clients will have more than one enrollment event
-    df = df[df.duplicated(subset=['full_name'], keep=False)]
     df = df.reset_index()
-    # convert to datetime for date operations
-    df["actual_date"] = pd.to_datetime(df.actual_date)
-    df["end_date"] = pd.to_datetime(df.end_date)
-    # rename for legibility
-    df = df.rename(columns={'actual_date.1': 'start_time', 'end_date.1': 'end_time'})
 
-    by_client = df.groupby('full_name')
-    curr_date = date.today().strftime('%b_%Y').lower()
-    time_format = '%Y-%m-%d %I:%M %p'
+    curr_date = (date.today().replace(day=1) - timedelta(days=1)).strftime('%b_%Y').lower()
     crisis_src = pd.read_excel('D:/KHIT docs/KHIT-scripts/crisis_src/crisis_sfy_2021.xlsx')
-
-    # for each client
-    for client, frame in by_client:
-        # get the 2 most recent enrollments
-        frame = frame.tail(2)
-        frame = frame.reset_index()
-
-        start_date = str(frame.loc[1, 'actual_date'])[0:10] + ' ' + str(frame.loc[1, 'start_time'])
-        end_date = str(frame.loc[0, 'end_date'])[0:10] + ' ' + str(frame.loc[0, 'end_time'])
-        print(start_date, end_date)
-        tdelta = datetime.strptime(end_date, time_format) - datetime.strptime(start_date, time_format)
-        # if there were fewer than 30 days between the 2, increment row 21 of the megareport
-        if tdelta.days < 30:
-            crisis_src.loc[19, curr_date] = crisis_src.loc[19, curr_date] + 1
-
+    # set row 21 to the length of the df
+    crisis_src.loc[19, curr_date] = len(df)
     # sum the row
     crisis_src.loc[19, 'SFY 2021 Total'] = crisis_src.iloc[19, 4:16].sum()
 
